8ba2ff14216f1ebd0629cf66d20c1e228a41e48b
GQG <184416297@qq.com>
2018-10-20 09:58:19 +0800

    医药网项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Vue from 'vue';
+  import axios from 'axios';
+  Vue.prototype.$http=axios;
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="home">
+        一药网项目初始化
+    </div>
+</template>
