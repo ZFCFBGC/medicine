837e21fb3647719d44c681cc400db4ef095c4d5c
winter <1160166395@qq.com>
2018-10-23 11:43:17 +0800

    rem

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
   import axios from 'axios';
   import Mint from 'mint-ui';
   import 'mint-ui/lib/style.css';
+  import './css/base.css';
+  import './css/common.css';
+  import rem from './rem.js';
+  rem();
   Vue.use(Mint);
   Vue.prototype.$http=axios;
   export default {
